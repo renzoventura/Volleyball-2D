46ee5c91f812fb0d4bfb5c17a31106d9800a89ff
renzoventura <edrickventura1996@gmail.com>
2021-05-10 21:59:25 +1200

    added basic character movement

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,17 +2,15 @@ extends "res://scenes/TemplatePlayer.gd"
 
 const SPEED : int = 200;
 const MAX_SPEED : int = 50;
-const JUMP : int = 4000;
+const JUMP : int = 30;
 const MAX_FALL_SPEED : int = 200;
-const GRAVITY : float = 9.8;
+const GRAVITY : float = 40.0;
 var motion : Vector2 = Vector2(0,0)
 var motion_up : Vector2 = Vector2(0,-1)
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
-
-
 
 func _process(delta):
 	move()
@@ -29,18 +27,15 @@ func move() -> void:
 		motion.x = SPEED
 	else:
 		motion.x = 0
-		
 
 func apply_gravity() -> void:
-	if(motion.y < MAX_FALL_SPEED):
-		motion.y = GRAVITY
-#	if is_on_floor() and motion_up.y > 0: 
-#		motion_up.y = 0
-#	elif is_on_ceiling():
-#		motion_up.y = 1
-#	else:
-#		if(motion_up.y < MAX_FALL_SPEED):
-#			motion_up.y += GRAVITY
+	if is_on_floor() and motion_up.y > 0: 
+		motion_up.y = 0
+	elif is_on_ceiling():
+		motion_up.y = 1
+	else:
+		if(motion_up.y < MAX_FALL_SPEED):
+			motion_up.y += GRAVITY
 	
 func jump() -> void:
 	if (Input.is_action_just_pressed("jump")):

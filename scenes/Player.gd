5b3e78dc97945f06ecadf66be987f0526a7bf06f
renzoventura <edrickventura1996@gmail.com>
2021-05-10 21:48:39 +1200

    Initial Project Commit

---
+++ scenes/Player.gd
@@ -0,0 +1,48 @@
+extends "res://scenes/TemplatePlayer.gd"
+
+const SPEED : int = 200;
+const MAX_SPEED : int = 50;
+const JUMP : int = 4000;
+const MAX_FALL_SPEED : int = 200;
+const GRAVITY : float = 9.8;
+var motion : Vector2 = Vector2(0,0)
+var motion_up : Vector2 = Vector2(0,-1)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	move()
+	jump()
+	apply_gravity()
+	move_and_slide(motion, motion_up)
+
+func move() -> void:
+	if (Input.is_action_pressed("left")):
+		print("Moving left");
+		motion.x = -SPEED
+	elif (Input.is_action_pressed("right")):
+		print("Moving right");
+		motion.x = SPEED
+	else:
+		motion.x = 0
+		
+
+func apply_gravity() -> void:
+	if(motion.y < MAX_FALL_SPEED):
+		motion.y = GRAVITY
+#	if is_on_floor() and motion_up.y > 0: 
+#		motion_up.y = 0
+#	elif is_on_ceiling():
+#		motion_up.y = 1
+#	else:
+#		if(motion_up.y < MAX_FALL_SPEED):
+#			motion_up.y += GRAVITY
+	
+func jump() -> void:
+	if (Input.is_action_just_pressed("jump")):
+		print("jump!")
+		motion.y = -JUMP
